c897b8e3811ad9e5e176eae69730d57f5aa54ce1
vijaya1406 <shonathakur14@gmail.com>
2022-10-20 20:59:10 +1100

    Saved calc program

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,29 @@
-year = int(input('Enter the year you want to check : '))
+ input year
+year = int(input("Which year do you want to check? "))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a leap year.")
+# define condition 1
+def leap1(year):
+ return (year % 4)
+# assign condition 1
+a= leap1(year)
+
+# define condition 2 to a
+def leap2(year):
+ return(year % 100)
+
+# assign condition 2 to b
+b= leap2(year)
+
+# define condition 3 to c
+def leap3(year):
+ return(year % 400)
+# assign condition 3
+c= leap3(year)
+
+# If the conditions are true (leap year), if not (not a leap year)
+if a ==0:
+ print("its a leap year")
+elif b ==0 and c ==0:
+ print("its a leap year")
 else:
-    print(year, "is not a leap year.")
+ print("not a leap year")
