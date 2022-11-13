b4ea1d1fb36890dbb99d479b307e30560c2e39bb
vijaya1406 <shonathakur14@gmail.com>
2022-11-13 12:51:51 +1100

    for_Even_numbers

---
+++ Angry_Goblin_hunt.py
@@ -0,0 +1,9 @@
+print("Welcome to the Angry Goblin Hunt")
+print("An award-winning game full of adventure and excitement (!)")
+name = input ("type in your name")
+print(f"{name}, do you think you can find the goblin hiding in the kitchen cupboards?")
+print("|_| |_| |_| |_| |_|" )
+cupboard_number = input("Which cupboard do you think the goblin is in [type in number]")
+
+
+
---
+++ print_odd_numbers.py
@@ -0,0 +1,9 @@
+numbers = range(100)
+
+numbers
+
+range(0,100)
+
+for number in numbers :
+    if number % 2!=0:
+        print (number)
