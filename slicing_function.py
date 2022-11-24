e146748a49cbed93701985da14f831d37fdcc888
vijaya1406 <shonathakur14@gmail.com>
2022-11-24 19:06:21 +1100

    Merge remote-tracking branch 'origin/feature_while_loop' into feature_while_loop
    # Conflicts:
    #	print_odd_numbers.py

---
+++ slicing_function.py
@@ -0,0 +1,7 @@
+#a = ['spam', 'milk', 'apple', 'cheese', 'bread']
+#Indices = [0, 1, 2, 3, 4]
+
+a= ['spam', 'milk', 'apple', 'cheese', 'bread']
+print (f"second to fourth numbers are {a[2:4]}")
+print (f"first to fifth numbers are {a[1:5]}")
+print (f"first and third ratio is {a[1:3]}")
